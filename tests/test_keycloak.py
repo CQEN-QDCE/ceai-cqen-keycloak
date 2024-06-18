381b17870f8d7c81a848e2e02849e2428939ccf1
Chrispel Simon Francis Tsambou Sonkeng <chrispelsimonfrancis.tsambousonkeng@mcn.gouv.qc.ca>
2024-06-18 10:29:41 -0400

    ajout de tests
    Signed-off-by: Chrispel Simon Francis Tsambou Sonkeng <chrispelsimonfrancis.tsambousonkeng@mcn.gouv.qc.ca>

--- tests/test_keycloak.py
+++ tests/test_keycloak.py
@@ -5,22 +5,27 @@ BASE_URL = 'http://localhost:8080'
 REALM_NAME = 'master'
 REALM_NAME_IMPORT = 'ceai'
 REALM_NAME_PASSWORD = 'ceai'
-CLIENT_ID = 'test'
-CLIENT_SECRET = 'KGXDTjxWfdrEczIIHiwqhtGranJuyDEU'
-USERNAME = 'test'
-PASSWORD = 'test'
+USERNAME = 'admin'
+PASSWORD = 'cqen'
 EXPECTED_LOGIN_THEME = 'cqen'
 EXPECTED_EMAIL_THEME = 'cqen'
 EXPECTED_ACCOUNT_THEME = 'cqen'
+ADMIN_CLIENT_ID = 'admin-cli'
+NEW_CLIENT_ID1 = 'test-client1'
+NEW_CLIENT_ID2 = 'test-client2'
+NEW_CLIENT_ID3 = 'test-client3'
+NEW_CLIENT_ID4 = 'test-client4'
+NEW_CLIENT_ID5 = 'test-client5'
+NEW_CLIENT_ID6 = 'test-client6'
+NEW_CLIENT_ID7 = 'test-client7'
 
 
-def get_access_token():
-    url = f'{BASE_URL}/realms/{REALM_NAME}/protocol/openid-connect/token'
+def get_admin_token(base_url, realm_name, client_id, username, password):
+    url = f'{base_url}/realms/{realm_name}/protocol/openid-connect/token'
     payload = {
-        'client_id': CLIENT_ID,
-        'client_secret': CLIENT_SECRET,
-        'username': USERNAME,
-        'password': PASSWORD,
+        'client_id': client_id,
+        'username': username,
+        'password': password,
         'grant_type': 'password'
     }
     response = requests.post(url, data=payload)
@@ -28,88 +33,160 @@ def get_access_token():
     return response.json()['access_token']
 
 
+def get_access_token_with_refresh(base_url, realm_name, client_id, client_secret, username, password):
+    token_url = f"{base_url}/realms/{realm_name}/protocol/openid-connect/token"
+    payload = {
+        'grant_type': 'client_credentials',
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'username': username,
+        'password': password,
+        'grant_type': 'password'
+    }
+    response = requests.post(token_url, data=payload)
+    print(f"Requête vers {token_url} avec payload {payload}")
+    print(f"Réponse : {response.status_code}, {response.text}")
+    response.raise_for_status()
+    return response.json()['access_token'], response.json().get('refresh_token')
+
+
+def refresh_access_token(base_url, realm_name, client_id, client_secret, refresh_token):
+    token_url = f"{base_url}/realms/{realm_name}/protocol/openid-connect/token"
+    payload = {
+        'grant_type': 'refresh_token',
+        'refresh_token': refresh_token,
+        'client_id': client_id,
+        'client_secret': client_secret
+    }
+    response = requests.post(token_url, data=payload)
+    response.raise_for_status()
+    return response.json()['access_token']
+
+def create_client(base_url, realm_name, client_id):
+    admin_token = get_admin_token(base_url, realm_name, ADMIN_CLIENT_ID, USERNAME, PASSWORD)
+    url = f'{base_url}/admin/realms/{realm_name}/clients'
+    headers = {
+        'Authorization': f'Bearer {admin_token}',
+        'Content-Type': 'application/json'
+    }
+    payload = {
+        'clientId': client_id,
+        'directAccessGrantsEnabled': True,
+        'publicClient': False
+    }
+    response = requests.post(url, json=payload, headers=headers)
+    response.raise_for_status()
+
+    client_uuid = None
+    clients_url = f'{base_url}/admin/realms/{realm_name}/clients'
+    response = requests.get(clients_url, headers=headers)
+    response.raise_for_status()
+    for client in response.json():
+        if client['clientId'] == client_id:
+            client_uuid = client['id']
+            break
+
+    client_secret_url = f'{base_url}/admin/realms/{realm_name}/clients/{client_uuid}/client-secret'
+    response = requests.get(client_secret_url, headers=headers)
+    response.raise_for_status()
+    client_secret = response.json()['value']
+
+    return client_id, client_secret
+
+def delete_client(base_url, realm_name, client_id, access_token):
+    url = f'{base_url}/admin/realms/{realm_name}/clients/{client_id}'
+    # client_token = get_client_acces_token(base_url, realm_name, client_id, client_secret, USERNAME, PASSWORD)
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    response = requests.delete(url, headers=headers)
+    response.raise_for_status()
+
+
+# client_id, client_secret = create_client(BASE_URL, REALM_NAME, NEW_CLIENT_ID)
+# CLIENT_TOKEN = get_client_acces_token(BASE_URL, REALM_NAME, client_id, client_secret, USERNAME, PASSWORD)
+
+
 def test_realm_import():
-    token = get_access_token()
+    client_id, client_secret = create_client(BASE_URL, REALM_NAME, NEW_CLIENT_ID1)
+    CLIENT_TOKEN, REFRESH_TOKEN = get_access_token_with_refresh(BASE_URL, REALM_NAME, client_id, client_secret, USERNAME, PASSWORD)
     url = f"{BASE_URL}/admin/realms/{REALM_NAME_IMPORT}"
     headers = {
-        'Authorization': f'Bearer {token}'
+        'Authorization': f'Bearer {CLIENT_TOKEN}'
     }
     response = requests.get(url, headers=headers)
     assert response.status_code == 200, f"Expected status code 200, got {response.status_code}"
     realm_data = response.json()
     assert realm_data['realm'] == REALM_NAME_IMPORT, f"Expected realm name '{REALM_NAME_IMPORT}', got '{realm_data['realm']}'"
     assert realm_data['enabled'] is True, "Expected realm to be enabled"
+    refresh_token = refresh_access_token(BASE_URL, REALM_NAME, client_id, client_secret, REFRESH_TOKEN)
+    delete_client(BASE_URL, REALM_NAME, client_id, refresh_token)
 
 
 def test_password_policy():
-    token = get_access_token()
+    client_id2, client_secret2 = create_client(BASE_URL, REALM_NAME, NEW_CLIENT_ID2)
+    CLIENT_TOKEN, REFRESH_TOKEK = get_access_token_with_refresh(BASE_URL, REALM_NAME, client_id2, client_secret2, USERNAME, PASSWORD)
     url = f'{BASE_URL}/admin/realms/{REALM_NAME_PASSWORD}/users'
     headers = {
-        'Authorization': f'Bearer {token}',
+        'Authorization': f'Bearer {CLIENT_TOKEN}',
         'Content-Type': 'application/json'
     }
-    # Exemple de mot de passe interdit
     forbidden_password = 'password123'
     payload = {
-        'username': 'testuser7',
+        'username': 'testuser',
         'enabled': True,
         'firstName': 'Test',
         'lastName': 'User',
-        'email': 'testuser7@example.com',
+        'email': 'testuser@example.com',
         'credentials': [{'type': 'password', 'value': forbidden_password, 'temporary': False}]
     }
     response = requests.post(url, json=payload, headers=headers)
-    # On s'attend à ce que la création de l'utilisateur échoue à cause de la politique de mot de passe
     assert response.status_code != 201, f"Expected failure, but got {response.status_code}: {response.text}"
-    # Vérifier le message d'erreur pour la politique de mot de passe
-    assert 'Password policy not met' in response.text, "Password policy violation message not found"
+    assert 'password is blacklisted' in response.text, "Password policy violation message not found"
 
 
 def test_login_theme_import():
-    token = get_access_token()
+    client_id, client_secret = create_client(BASE_URL, REALM_NAME, NEW_CLIENT_ID3)
+    CLIENT_TOKEN, REFRESH_TOKEN = get_access_token_with_refresh(BASE_URL, REALM_NAME, client_id, client_secret, USERNAME, PASSWORD)
     url = f'{BASE_URL}/admin/realms/{REALM_NAME_IMPORT}'
     headers = {
-        'Authorization': f'Bearer {token}'
+        'Authorization': f'Bearer {CLIENT_TOKEN}'
     }
     response = requests.get(url, headers=headers)
     assert response.status_code == 200, f"Expected status code 200, got {response.status_code}"
     realm_data = response.json()
-    # Vérifier si la clé 'loginTheme' existe dans le dictionnaire realm_data
     login_theme = realm_data.get('loginTheme')
     assert login_theme is not None, "Expected 'loginTheme' key in realm data, but it does not exist"
-    # Maintenant, vous pouvez effectuer votre assertion
     assert login_theme == EXPECTED_LOGIN_THEME, f"Expected login theme '{EXPECTED_LOGIN_THEME}', got '{login_theme}'"
 
 
 def test_account_theme_import():
-    token = get_access_token()
+    client_id, client_secret = create_client(BASE_URL, REALM_NAME, NEW_CLIENT_ID4)
+    CLIENT_TOKEN, REFREH_TOKEN = get_access_token_with_refresh(BASE_URL, REALM_NAME, client_id, client_secret, USERNAME, PASSWORD)
     url = f'{BASE_URL}/admin/realms/{REALM_NAME_IMPORT}'
     headers = {
-        'Authorization': f'Bearer {token}'
+        'Authorization': f'Bearer {CLIENT_TOKEN}'
     }
     response = requests.get(url, headers=headers)
     assert response.status_code == 200, f"Expected status code 200, got {response.status_code}"
     realm_data = response.json()
-    # Vérifier si la clé 'accountTheme' existe dans le dictionnaire realm_data
     account_theme = realm_data.get('accountTheme')
     assert account_theme is not None, "Expected 'accountTheme' key in realm data, but it does not exist"
-    # Maintenant, vous pouvez effectuer votre assertion
     assert account_theme == EXPECTED_ACCOUNT_THEME, f"Expected account theme '{EXPECTED_ACCOUNT_THEME}', got '{account_theme}'"
 
 
 def test_email_theme_import():
-    token = get_access_token()
+    client_id, client_secret = create_client(BASE_URL, REALM_NAME, NEW_CLIENT_ID5)
+    CLIENT_TOKEN, REFREH_TOKEN = get_access_token_with_refresh(BASE_URL, REALM_NAME, client_id, client_secret, USERNAME, PASSWORD)
     url = f'{BASE_URL}/admin/realms/{REALM_NAME_IMPORT}'
     headers = {
-        'Authorization': f'Bearer {token}'
+        'Authorization': f'Bearer {CLIENT_TOKEN}'
     }
     response = requests.get(url, headers=headers)
     assert response.status_code == 200, f"Expected status code 200, got {response.status_code}"
     realm_data = response.json()
-    # Vérifier si la clé 'emailTheme' existe dans le dictionnaire realm_data
     email_theme = realm_data.get('emailTheme')
     assert email_theme is not None, "Expected 'emailTheme' key in realm data, but it does not exist"
-    # Maintenant, vous pouvez effectuer votre assertion
     assert email_theme == EXPECTED_EMAIL_THEME, f"Expected email theme '{EXPECTED_EMAIL_THEME}', got '{email_theme}'"
 
 
