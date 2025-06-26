73d3b5005a21630b57192c75580ff3afa64182f2
pierre.jiji <pierre.jiji@mcn.gouv.qc.ca>
2025-06-26 15:00:17 -0400

    Écrire le desc en francais
    Signed-off-by: pierre.jiji <pierre.jiji@mcn.gouv.qc.ca>

--- argocd/apps/variables.tf
+++ argocd/apps/variables.tf
@@ -1,115 +1,113 @@
 # tflint-ignore: terraform_unused_declarations
 variable "cluster_name" {
-  description = "Name of cluster"
+  description = "Nom du cluster"
   type        = string
 
   validation {
     condition     = length(var.cluster_name) > 0 && length(var.cluster_name) <= 19
-    error_message = "The cluster name must be between [1, 19] characters"
+    error_message = "Le nom du cluster doit comporter entre 1 et 19 caractères"
   }
   default = "cluster-eks"
-
 }
 
 variable "cluster_region" {
-  description = "Region to create the cluster"
+  description = "Région où créer le cluster"
   type        = string
   default     = "ca-central-1"
 }
 
 variable "aws_profile" {
   type        = string
-  description = "Optional: If an SSO connection is being used, specify the SSO profile name in the .aws/config file on the machine executing the deployment."
+  description = "Optionnel : Si une connexion SSO est utilisée, spécifiez le nom du profil SSO dans le fichier .aws/config sur la machine exécutant le déploiement."
   default     = null
 }
 
 variable "assume_role_arn" {
   type        = string
-  description = "The ARN of the role to assume"
+  description = "L'ARN du rôle à assumer"
   default     = null
 }
 
 variable "workload_account_type" {
   type        = string
-  description = "Name of system environment deployed on LZA AWS"
+  description = "Nom de l'environnement système déployé sur AWS LZA"
   default     = "Sandbox"
 
   validation {
     condition     = contains(["Sandbox", "Dev", "Prod"], var.workload_account_type)
-    error_message = "workload_account_type must be one of: Sandbox, Development, Production"
+    error_message = "workload_account_type doit être l'une des valeurs suivantes : Sandbox, Development, Production"
   }
 }
 
 variable "project_name" {
-  description = "project for Keycloak"
+  description = "Projet pour Keycloak"
   type        = string
 }
 
 variable "acm_certificate_arn" {
-  description = "ACM certificate ARN for the load balancer"
+  description = "ARN du certificat ACM pour le load balancer"
   type        = string
 }
 
 variable "server_image_keycloak" {
-  description = "value of the image repo for the Keycloak server"
+  description = "Valeur du dépôt d'image pour le serveur Keycloak"
   type        = string
   default     = "quay.io/keycloak/keycloak"
 }
 
 variable "image_tag_keycloak" {
-  description = "value of the image tag for the Keycloak server"
+  description = "Valeur du tag d'image pour le serveur Keycloak"
   type        = string
   default     = "26.2.4"
 }
 
 variable "chart_path_keycloak" {
-  description = "The path to the chart in the repository for the Keycloak server"
+  description = "Chemin du chart dans le dépôt pour le serveur Keycloak"
   type        = string
   default     = "charts"
 }
 
 variable "repo_github_url" {
-  description = "The URL of the repository for Keycloak"
+  description = "URL du dépôt GitHub pour Keycloak"
   type        = string
   default     = "https://github.com/CQEN-QDCE/ceai-cqen-keycloak"
 }
 
 variable "target_revision" {
-  description = "The target revision of the repository for Keycloak"
+  description = "Révision cible du dépôt pour Keycloak"
   type        = string
   default     = "feature/helmcharts"
 }
 
-
 variable "host_path_keycloak" {
-  description = "Host path for Keycloak"
+  description = "Chemin d'hôte pour Keycloak"
   type        = string
   default     = "keyclaok-preprod.asea.cqen.ca"
 }
 
 variable "replica_count_keycloak" {
-  description = "Number of replicas for Keycloak"
+  description = "Nombre de réplicas pour Keycloak"
   type        = number
   default     = 1
 }
 
 variable "endpoint_bd_keycloak" {
-  description = "Endpoint for the Keycloak database"
+  description = "Endpoint pour la base de données Keycloak"
   type        = string
 }
 
 variable "keycloak_db_name" {
-  description = "Keycloak database name"
+  description = "Nom de la base de données Keycloak"
   type        = string
   default     = "keycloak-xroad-default"
 }
 
 variable "keycloak_db_admin_user" {
-  description = "Keycloak database admin user"
+  description = "Utilisateur administrateur de la base de données Keycloak"
   type        = string
 }
 
 variable "keycloak_db_admin_password" {
-  description = "Keycloak database admin password"
+  description = "Mot de passe administrateur de la base de données Keycloak"
   type        = string
 }
