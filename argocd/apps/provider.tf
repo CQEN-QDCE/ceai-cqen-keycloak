0b42ca81941179cd7278dcfec7ead25a0e260b56
Pierre Jiji <pierre.jiji@mcn.gouv.qc.ca>
2025-06-30 11:59:36 -0400

    Enlever la commentaire
    Co-authored-by: Rocio Philipps <rocio.philippsvalencia@mcn.gouv.qc.ca>
    Signed-off-by: Pierre Jiji <pierre.jiji@mcn.gouv.qc.ca>

--- argocd/apps/provider.tf
+++ argocd/apps/provider.tf
@@ -27,7 +27,6 @@ data "aws_eks_cluster" "cluster" {
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  # token                  = data.aws_eks_cluster_auth.auth.token
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
