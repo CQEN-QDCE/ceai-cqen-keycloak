0467e5b753c75b9fffb61d038d2a0d78a2cff10c
Chrispel Simon Francis Tsambou Sonkeng <chrispelsimonfrancis.tsambousonkeng@mcn.gouv.qc.ca>
2025-10-06 16:16:48 -0400

    correction du client secret
    Signed-off-by: Chrispel Simon Francis Tsambou Sonkeng <chrispelsimonfrancis.tsambousonkeng@mcn.gouv.qc.ca>

--- test-api/test_api_keycloak.py
+++ test-api/test_api_keycloak.py
@@ -106,7 +106,7 @@ def test_04_oidc_flow_is_functional():
     # Utilisez ici un client/secret d'une application existante dans le realm cible
     data = {
         'client_id': 'api-service-client', 
-        'client_secret': 'votre_secret_ci_test',
+        'client_secret': 'abf3KdSWyg4RzDPFXOD6XdXNbNZgNmzA',
         'grant_type': 'client_credentials'
     }
     
